d79f559ab83dcf3b4ada816f6a8bbb22a59ae232
cudlilss <kit.sh@icloud.com>
2025-04-27 16:41:27 +0300

    ну пока все

--- front/main/script.js
+++ front/main/script.js
@@ -1,53 +1,8 @@
-
-let a = 0
-a++
-// + или -- прибавляет или отнимает единицу
-console.log(a)
-console.log(String(a))
-// пример преобразования числа в строку, также можно сделать с Number и Boolean НО с number можно сделать +
-
-console.log(
-    +'134'
-)
-// тоби ж я из строки сделал число
-
-// 0 NaN null undefined '' - это все false, остальное - это все true
-
-let sms = 'ку ку'
-console.log(sms)
-sms += 'салам алейкум'
-console.log(sms)
-
-// вот как добавить что то в строку Которая уже есть
-
-const sms2 = prompt('ку ку','ку')
-if (sms2 === ''){
-console.log('Ну и ладно')
-}
-else{
-    console.log(`Ответ на сообщение : ${sms2}`)
-}
-const sms3 = confirm(sms2)
-if (sms3 === true){
-console.log('ты нажал ок')
-}
-else{
-    console.log('ты нажал отмена')
-}
-// пример как сделать всплывающие окно с вопросом полем для ввода ответа
-// и проверкой введенного ответа
+let catalog = false
 
 
-// let i = 0 можно и так но в начале (; надо так и т.д.)
-//for (let i = 0; i < 10; i++) {
-//   alert(i)
-//}
 
-
-function showMessage(text, num){
-    for (let i = 0; i < num; i++) {
-        console.log(`${text} ${name}`)
-    }
-}
-
-showMessage('hello', 10)
+document.getElementById("tg_bot_button_catalog").onclick = function() {
+  catalog = true ;
+  console.log("Значение переменной успешно изменено!");
+};
