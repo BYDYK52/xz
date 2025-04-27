6b8888489fdf74a9bac3b72d674a93fe3d440019
cudlilss <kit.sh@icloud.com>
2025-04-27 17:34:10 +0300

    победа

--- front/main/script.js
+++ front/main/script.js
@@ -1,8 +1,33 @@
-let catalog = false
-
-
+//let catalog = false
+//
+//
+//
+//document.getElementById("tg_bot_button_catalog").onclick = function() {
+//  catalog = true ;
+//  console.log("Значение переменной успешно изменено!");
+//};
+//
+//
+//<script>
+//  document.getElementById('tg_bot_button_catalog').addEventListener('click', function() {
+//    const catalogTitle = document.querySelector('.catalog_title');
+//    if (catalogTitle.style.display === 'none' || catalogTitle.style.display === '') {
+//      catalogTitle.style.display = 'block'; // показываем элемент
+//    } else {
+//      catalogTitle.style.display = 'none'; // скрываем элемент (если нужно toggle)
+//    }
+//  });
+//</script>
+let catalog = false;
 
 document.getElementById("tg_bot_button_catalog").onclick = function() {
-  catalog = true ;
-  console.log("Значение переменной успешно изменено!");
+  catalog = !catalog;
+  console.log("Значение переменной catalog изменено на:", catalog);
+
+  const catalogTitle = document.querySelector('.catalog_title');
+  if (catalog) {
+    catalogTitle.style.display = 'block';
+  } else {
+    catalogTitle.style.display = 'none';
+  }
 };
